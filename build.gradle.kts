c6e17aed056b293747230c31a6f07cebac143ebd
Wadey <83308534+Wadey3636@users.noreply.github.com>
2025-03-31 13:30:50 -0700

    Update build.gradle.kts
    Stopped shadowing essential.
    Much better load times and size reduction.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,4 @@
 import org.apache.commons.lang3.SystemUtils
-import org.jetbrains.kotlin.ir.backend.js.compile
 
 plugins {
     idea
@@ -10,8 +9,6 @@ plugins {
     kotlin("jvm") version "2.0.0"
 }
 
-//Constants:
-
 val baseGroup: String by project
 val mcVersion: String by project
 val version: String by project
@@ -19,17 +16,16 @@ val mixinGroup = "$baseGroup.mixin"
 val modid: String by project
 val transformerFile = file("src/main/resources/accesstransformer.cfg")
 
-// Toolchains:
+
 java {
     toolchain.languageVersion.set(JavaLanguageVersion.of(8))
 }
 
-// Minecraft configuration:
+
 loom {
     log4jConfigs.from(file("log4j2.xml"))
     launchConfigs {
         "client" {
-            // If you don't want mixins, remove these lines
             property("mixin.debug", "true")
             arg("--tweakClass", "org.spongepowered.asm.launch.MixinTweaker")
         }
@@ -39,26 +35,23 @@ loom {
             if (SystemUtils.IS_OS_MAC_OSX) {
                 // This argument causes a crash on macOS
                 vmArgs.remove("-XstartOnFirstThread")
-
             }
         }
         remove(getByName("server"))
     }
     forge {
         pack200Provider.set(dev.architectury.pack200.java.Pack200Adapter())
-        // If you don't want mixins, remove this lines
+
         mixinConfig("mixins.$modid.json")
-	    if (transformerFile.exists()) {
-			println("Installing access transformer")
-		    accessTransformer(transformerFile)
-	    }
+        if (transformerFile.exists()) {
+            println("Installing access transformer")
+            accessTransformer(transformerFile)
+        }
     }
-    // If you don't want mixins, remove these lines
     mixin {
         defaultRefmapName.set("mixins.$modid.refmap.json")
     }
 }
-
 
 tasks.compileJava {
     dependsOn(tasks.processResources)
@@ -69,15 +62,11 @@ sourceSets.main {
     java.srcDir(layout.projectDirectory.dir("src/main/kotlin"))
     kotlin.destinationDirectory.set(java.destinationDirectory)
 }
-
-// Dependencies:
-
 repositories {
     mavenCentral()
 
     maven("https://repo.essential.gg/public")
     maven("https://repo.spongepowered.org/maven/")
-    // If you don't want to log in with your real minecraft account, remove this line
     maven("https://pkgs.dev.azure.com/djtheredstoner/DevAuth/_packaging/public/maven/v1")
 }
 
@@ -87,44 +76,30 @@ val shadowImpl: Configuration by configurations.creating {
 
 
 dependencies {
-    // Move these from implementation to shadowImpl to ensure they're included in the final jar
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
-    implementation("org.jetbrains.kotlin:kotlin-reflect:1.9.0")
-
-
     shadowImpl("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
     shadowImpl("org.jetbrains.kotlin:kotlin-reflect:1.9.0")
-
-    // Keep your existing shadowImpl
     shadowImpl(kotlin("stdlib-jdk8"))
 
-    // Keep the rest of your dependencies as they are
-    compileOnly("com.github.NotEnoughUpdates:NotEnoughUpdates:2.4.0:all")
-    implementation("gg.essential:loader-launchwrapper:1.1.3")
-    implementation("com.mojang:brigadier:1.2.9")
-    implementation("com.github.Stivais:Commodore:bea320fe0a")
-    compileOnly("gg.essential:essential-1.8.9-forge:12132+g6e2bf4dc5")
-
     shadowImpl("gg.essential:loader-launchwrapper:1.1.3")
-    shadowImpl("gg.essential:essential-1.8.9-forge:12132+g6e2bf4dc5")
+    implementation("gg.essential:essential-1.8.9-forge:12132+g6e2bf4dc5") {
+        exclude(module = "asm")
+        exclude(module = "asm-commons")
+        exclude(module = "asm-tree")
+        exclude(module = "gson")
+        exclude(module = "vigilance")
+    }
+
     shadowImpl("com.mojang:brigadier:1.2.9")
-    shadowImpl("com.github.Stivais:Commodore:bea320fe0a")
     minecraft("com.mojang:minecraft:1.8.9")
     mappings("de.oceanlabs.mcp:mcp_stable:22-1.8.9")
     forge("net.minecraftforge:forge:1.8.9-11.15.1.2318-1.8.9")
-
-    // Keep your mixin configuration
     shadowImpl("org.spongepowered:mixin:0.7.11-SNAPSHOT") {
         isTransitive = false
     }
     annotationProcessor("org.spongepowered:mixin:0.8.5-SNAPSHOT")
 
     runtimeOnly("me.djtheredstoner:DevAuth-forge-legacy:1.2.1")
-
 }
-
-
-// Tasks:
 
 tasks.withType(JavaCompile::class) {
     options.encoding = "UTF-8"
@@ -135,12 +110,10 @@ tasks.withType(org.gradle.jvm.tasks.Jar::class) {
     manifest.attributes.run {
         this["FMLCorePluginContainsFMLMod"] = "true"
         this["ForceLoadAsMod"] = "true"
-
-        // If you don't want mixins, remove these lines
         this["TweakClass"] = "org.spongepowered.asm.launch.MixinTweaker"
         this["MixinConfigs"] = "mixins.$modid.json"
-	    if (transformerFile.exists())
-			this["FMLAT"] = "${modid}_at.cfg"
+        if (transformerFile.exists())
+            this["FMLAT"] = "${modid}_at.cfg"
     }
 }
 
@@ -179,7 +152,6 @@ tasks.shadowJar {
         }
     }
 
-    // If you want to include other dependencies and shadow them, you can relocate them in here
     fun relocate(name: String) = relocate(name, "$baseGroup.deps.$name")
 }
 
@@ -209,4 +181,3 @@ tasks.jar {
 
 
 tasks.assemble.get().dependsOn(tasks.remapJar)
-
